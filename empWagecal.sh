2b4b301816f75d50a86d82b750c8b9c7d46a2e58
Amit0129 <amitkumarnayak40@gmail.com>
2022-09-21 16:18:18 +0530

    Added: UC5 Calculating Wages for a Month

--- empWagecal.sh
+++ empWagecal.sh
@@ -5,10 +5,15 @@ echo "Welcome to Employee Wage Computation Program on Master Branch"
 
 isPartTime=1;
 isFullTime=2;
-empCheck=$((RANDOM%3));
+totalSalary=0
 empRatePerHr=20;
+netWorkingDay=20;
 
-case $empCheck in
+
+for (( day=1; day<=20; day++ ))
+do
+        empCheck=$((RANDOM%3));
+        case $empCheck in
                 $isFullTime)
                         empHrs=8
                         ;;
@@ -16,10 +21,10 @@ case $empCheck in
                         empHrs=4
                         ;;
                 *)
-                	empHrs=0
+                empHrs=0
                         ;;
-esac
-salary=$(($empRatePerHr*$empHrs))
-
-
-echo $salary
+        esac
+        salary=$(($empRatePerHr*$empHrs))
+        totalSalary=$(($totalSalary+$salary))
+done
+echo $totalSalary
