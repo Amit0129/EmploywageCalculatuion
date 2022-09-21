6fbe0e0ec370794d98e965e721527c3e1fe5c24c
Amit0129 <amitkumarnayak40@gmail.com>
2022-09-21 15:39:52 +0530

    Added: UC2 Calculate Daily Employee Wage

--- empWagecal.sh
+++ empWagecal.sh
@@ -1,10 +1,17 @@
+#! /bin/bash -x
+
 echo "Welcome to Employee Wage Computation Program on Master Branch"
-isPresent=1
-randomCheck=$((RANDOM%2))
+
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
 if [ $isPresent -eq $randomCheck ]
 then
-        echo "Employ is present"
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empRatePerHr*$empHrs))
 else
-        echo "Employ is absent"
+	salary=0;
 fi
 
